c310496500ce82184908711c6c732daec43ac97b
adrioro <artistsvillage@me.com>
2019-02-01 11:26:21 -0500

    Third commit

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ $("#fullDate").click(function () {
     var d = new Date(); //How we start our new Date.
     $('#fullDate').after("<br><br>" + d + "<br>");
 })
+var d = new Date();
+$("#fullDay").click(function () {
+    var day = (d.getDay());
+    $('#fullDay').after("<br><br>" + day + "(Monday is 0)<br>");
+})
